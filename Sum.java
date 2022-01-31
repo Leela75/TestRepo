7b22ae55d76cdd91870a8d13f2e8662acb015365
Leela K S <leelaks92@gmail.com>
2022-01-31 19:33:43 +0530

    sum modification+ new diff file

--- Sum.java
+++ Sum.java
@@ -3,9 +3,13 @@ public class Sum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10 , b=70;
+		int a=10 , b=120;
 		int c=a+b;
-		System.out.println("The sum is " +c);
+		System.out.println("The Total added sum is " +c);
+                
+                System.out.println("The Total added sum is " +c);
+
+
 		
 	}
 
---
+++ diff.java
@@ -0,0 +1,12 @@
+
+public class diff {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10 , b=70;
+		int c=a-b;
+		System.out.println("The total difference is " +c);
+	}
+
+}
