4b99d111e630e86c10f075249eda7253a2188a48
Leela K S <leelaks92@gmail.com>
2022-01-31 19:05:03 +0530

    Sum program

---
+++ Sum.java
@@ -0,0 +1,12 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10 , b=70;
+		int c=a+b;
+		System.out.println("The sum is " +c);
+		
+	}
+
+}
