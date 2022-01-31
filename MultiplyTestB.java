88e3885a6a64108d1380b6440e3d50fdce904b0a
Leela K S <leelaks92@gmail.com>
2022-01-31 20:14:00 +0530

    New multiply file

---
+++ MultiplyTestB.java
@@ -0,0 +1,14 @@
+
+public class MultiplyTestB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10 , b=70;
+		int c=a*b;
+		System.out.println("The multiply value is " +c);
+
+
+	}
+
+}
